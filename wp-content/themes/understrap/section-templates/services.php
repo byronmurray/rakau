0311e074497dbbc66414f1d191baabd49a91034f
vagrant <vagrant@homestead>
2018-01-21 07:30:30 +0000

    service section - first commit

--- wp-content/themes/understrap/section-templates/services.php
+++ wp-content/themes/understrap/section-templates/services.php
@@ -1,3 +1,40 @@
 <section class="services">
-  hello world
+  <div class="box box1">
+    <div class="box-content-wrap">
+      <h3 class="box-content-header">Box one</h3>
+      <p class="box-content-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <a href="#" class="btn btn-primary box-content-link">Contact us</a>
+    </div>
+  </div>
+
+  <div class="box box2">
+
+    <div class="box-content-wrap">
+      <h3 class="box-content-header">Box two</h3>
+      <p class="box-content-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <a href="#" class="btn btn-primary box-content-link">Contact us</a>
+    </div>
+
+  </div>
+  <div class="box box3">
+    <div class="box-content-wrap">
+      <h3 class="box-content-header">Box three</h3>
+      <p class="box-content-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <a href="#" class="btn btn-primary box-content-link">Contact us</a>
+    </div>
+  </div>
+  <div class="box box4">
+    <div class="box-content-wrap">
+      <h3 class="box-content-header">Box four</h3>
+      <p class="box-content-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <a href="#" class="btn btn-primary box-content-link">Contact us</a>
+    </div>
+  </div>
+  <div class="box box5">
+    <div class="box-content-wrap">
+      <h3 class="box-content-header">Box five</h3>
+      <p class="box-content-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <a href="#" class="btn btn-primary box-content-link">Contact us</a>
+    </div>
+  </div>
 </section>
